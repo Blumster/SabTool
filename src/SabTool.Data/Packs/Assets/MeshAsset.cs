a92fc61ed529ca63c212eb30f030d3b2eabbc218
Blumster <sarkany0@hotmail.com>
2021-08-11 20:26:04 +0200

    Add MeshAsset

---
+++ src/SabTool.Data/Packs/Assets/MeshAsset.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SabTool.Data.Packs.Assets
+{
+    using Packs;
+    using Utils;
+    using Utils.Extensions;
+
+    public class MeshAsset : IStreamBlockAsset
+    {
+        public bool Read(MemoryStream data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Write(MemoryStream writer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Import(string filePath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Export(string outputPath)
+        {
+        }
+    }
+}
