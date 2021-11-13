69534428a6d1b36d28dd8c3b6de5959adf7c9aa3
Blumster <sarkany0@hotmail.com>
2021-11-13 02:42:38 +0100

    Add basic WIP material and its serializer

---
+++ src/SabTool.CLI/Commands/MaterialCategory.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace SabTool.CLI.Commands
+{
+    using Base;
+    using Serializers.Graphics;
+
+    public class MaterialCategory : BaseCategory
+    {
+        public const string MaterialsRawFileName = "France.materials";
+        public const string MaterialsJsonFileName = "materials.json";
+
+        public override string Key => "materials";
+
+        public override string Shortcut => "ma";
+
+        public override string Usage => "<sub command>";
+
+        public class UnpackCommand : BaseCommand
+        {
+            public override string Key { get; } = "unpack";
+            public override string Shortcut { get; } = "u";
+            public override string Usage { get; } = "<game base path> <output directory>";
+
+            public override bool Execute(IEnumerable<string> arguments)
+            {
+                if (arguments.Count() < 2)
+                {
+                    Console.WriteLine("ERROR: Not enough arguments given!");
+                    return false;
+                }
+
+                var basePath = arguments.ElementAt(0);
+                var outputDir = arguments.ElementAt(1);
+
+                if (!Directory.Exists(basePath))
+                {
+                    Console.WriteLine("ERROR: The game base path does not exist!");
+                    return false;
+                }
+
+                Directory.CreateDirectory(outputDir);
+
+                var materialsFilePath = Path.Combine(basePath, MaterialsRawFileName);
+                if (!File.Exists(materialsFilePath))
+                {
+                    Console.WriteLine($"ERROR: Materials file could not be found under \"{materialsFilePath}\"!");
+                    return false;
+                }
+
+                using var inFileStream = new FileStream(materialsFilePath, FileMode.Open, FileAccess.Read, FileShare.Read);
+
+                var materials = MaterialSerializer.DeserializeRaw(inFileStream);
+
+                using var outFileStream = new FileStream(Path.Combine(outputDir, MaterialsJsonFileName), FileMode.Create, FileAccess.Write, FileShare.None);
+
+                MaterialSerializer.SerializeJSON(materials, outFileStream);
+
+                return true;
+            }
+        }
+
+        public class PackCommand : BaseCommand
+        {
+            public override string Key { get; } = "pack";
+            public override string Shortcut { get; } = "p";
+            public override string Usage { get; } = "<input file path> <output dir path>";
+
+            public override bool Execute(IEnumerable<string> arguments)
+            {
+                if (arguments.Count() < 2)
+                {
+                    Console.WriteLine("ERROR: Not enough arguments given!");
+                    return false;
+                }
+
+                var inputFilePath = arguments.ElementAt(0);
+                var outputDir = arguments.ElementAt(1);
+
+                if (!File.Exists(inputFilePath))
+                {
+                    Console.WriteLine("ERROR: The materials input file path does not exist!");
+                    return false;
+                }
+
+                Directory.CreateDirectory(outputDir);
+
+                using var inFileStream = new FileStream(inputFilePath, FileMode.Open, FileAccess.Read, FileShare.Read);
+
+                var materials = MaterialSerializer.DeserialzieJSON(inFileStream);
+
+                using var outFileStream = new FileStream(Path.Combine(outputDir, MaterialsRawFileName), FileMode.Create, FileAccess.Write, FileShare.None);
+
+                MaterialSerializer.SerializeRaw(materials, outFileStream);
+
+                return true;
+            }
+        }
+    }
+}
---
+++ src/SabTool.Data/Graphics/Material.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Generic;
+using System.Numerics;
+
+namespace SabTool.Data.Graphics
+{
+    using Utils;
+
+    public class Material
+    {
+        public static Dictionary<Crc, Material> Container { get; } = new();
+
+        public int Id { get; set; }
+        public byte Unk1 { get; set; }
+        public byte Unk2 { get; set; }
+        public Crc Texture { get; set; }
+        public Crc PassNameCrc { get; set; }
+        public uint Flags { get; set; }
+        public RenderState[] RenderStates { get; set; }
+        public TextureState[] TextureStates { get; set; }
+        public Unk3 Unk3Val { get; set; }
+        public int VertexShader { get; set; }
+        public ShaderParameter VertexShaderParameters { get; set; }
+        public int PixelShader { get; set; }
+        public ShaderParameter PixelShaderParameters { get; set; }
+
+        public List<Crc> Keys { get; set; } = new();
+
+        #region Subclasses
+        public enum RenderStateType
+        {
+            UnkC3 = 0xC3
+        }
+
+        public class RenderState
+        {
+            public RenderStateType Type { get; set; }
+            public uint Value { get; set; }
+        }
+
+        public enum TextureStateType
+        {
+
+        }
+
+        public class TextureState
+        {
+            public TextureStateType Type { get; set; }
+            public uint Value { get; set; }
+
+
+        }
+
+        public class Unk3
+        {
+            public byte Count { get; set; }
+            public byte[] UnkArray { get; set; }
+            public Vector4[] UnkVectors { get; set; }
+        }
+
+        public class ShaderParameter
+        {
+            public byte VectorCount { get; set; }
+            public byte IntCount { get; set; }
+            public byte BoolCount { get; set; }
+            public Vector4[] VectorParameters { get; set; }
+            public int[][] IntParameters { get; set; }
+            public bool[] BoolParameters { get; set; }
+        }
+
+        public class Pass
+        {
+            public Crc PassNameCrc { get; set; }
+            public uint Flags { get; set; }
+            public int RenderStateIndex { get; set; }
+            public int TextureStateIndex { get; set; }
+            public int Unk3Index { get; set; }
+            public int PixelParameterIndex { get; set; }
+            public int VertexParameterIndex { get; set; }
+            public int PixelShaderUnk { get; set; }
+            public int VertexShaderUnk { get; set; }
+        }
+        #endregion
+    }
+}
