4ae6876693343ffe43398f51849db4c2ac458e42
Blumster <sarkany0@hotmail.com>
2022-08-21 23:27:25 +0200

    Refactor texture exporting, should be 100% now

--- src/SabTool.Data/Packs/Assets/TextureAsset.cs
+++ src/SabTool.Data/Packs/Assets/TextureAsset.cs
@@ -10,7 +10,7 @@ public class TextureAsset
     public Crc NameCrc { get; }
     public string Name { get; set; }
 
-    public byte[][] DDSFiles { get; set; }
+    public byte[] DDSFile { get; set; }
 
     public TextureAsset(Crc nameCrc)
     {
@@ -24,31 +24,13 @@ public class TextureAsset
 
     public void Export(string outputPath)
     {
-        if (DDSFiles == null)
-            return;
-
-        if (DDSFiles.Length == 1)
+        if (DDSFile != null)
         {
             var outputFilePath = Path.Combine(outputPath, $"{Name}.dds");
 
             Directory.CreateDirectory(Path.GetDirectoryName(outputFilePath));
 
-            File.WriteAllBytes(outputFilePath, DDSFiles[0]);
-
-            return;
-        }
-
-        for (var i = 0; i < DDSFiles.Length; ++i)
-        {
-            if (DDSFiles[i] == null)
-                continue;
-
-            var localName = $"{Name}{i}.dds";
-            var outputFilePath = Path.Combine(outputPath, localName);
-
-            Directory.CreateDirectory(Path.GetDirectoryName(outputFilePath));
-
-            File.WriteAllBytes(outputFilePath, DDSFiles[i]);
+            File.WriteAllBytes(outputFilePath, DDSFile);
         }
     }
 }
