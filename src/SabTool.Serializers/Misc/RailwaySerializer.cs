91e201606d22d6dc41967080d61732c6e7e4c943
Blumster <sarkany0@hotmail.com>
2023-10-23 15:48:55 +0200

    Add railway names to hashes & refactor file

--- src/SabTool.Serializers/Misc/RailwaySerializer.cs
+++ src/SabTool.Serializers/Misc/RailwaySerializer.cs
@@ -1,114 +1,118 @@
-﻿using SabTool.Data.Misc;
-using SabTool.Utils;
-using SabTool.Utils.Extensions;
-using SharpGLTF.Geometry;
-using SharpGLTF.Geometry.VertexTypes;
-using SharpGLTF.Materials;
-using SharpGLTF.Scenes;
-using SharpGLTF.Schema2;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Numerics;
 using System.Text;
 
-namespace SabTool.Serializers.Misc
+using SharpGLTF.Geometry;
+using SharpGLTF.Geometry.VertexTypes;
+using SharpGLTF.Materials;
+using SharpGLTF.Scenes;
+using SharpGLTF.Schema2;
+
+namespace SabTool.Serializers.Misc;
+
+using SabTool.Data.Misc;
+using SabTool.Utils;
+using SabTool.Utils.Extensions;
+
+public class RailwaySerializer
 {
-    public class RailwaySerializer
+    public static List<RailwaySpline> DeserializeRaw(Stream stream)
     {
-        public static List<RailwaySpline> DeserializeRaw(Stream stream)
+        using var reader = new BinaryReader(stream, Encoding.ASCII, true);
+
+        if (!reader.CheckHeaderString("RAI6", reversed: true))
+            throw new Exception("Invalid RAI6 header found!");
+
+        var splineCount = reader.ReadInt32();
+
+        var result = new List<RailwaySpline>(splineCount);
+
+        foreach (var splineIndex in Enumerable.Range(0, splineCount))
         {
-            using BinaryReader reader = new(stream, Encoding.ASCII, true);
-
-            if (!reader.CheckHeaderString("RAI6", reversed: true))
+            var spline = new RailwaySpline()
             {
-                throw new Exception("Invalid RAI6 header found!");
-            }
-
-            int splineCount = reader.ReadInt32();
-
-            List<RailwaySpline> result = new()
-            {
-                Capacity = splineCount
+                Name = reader.ReadPrefixedString(),
+                NameCrc = new Crc(reader.ReadUInt32()),
+                Distance = reader.ReadSingle()
             };
 
+            // Store hashes
+            Hash.FNV32string(spline.Name);
+            Hash.StringToHash(spline.Name);
 
-            foreach (int splineIndex in Enumerable.Range(0, splineCount))
+            int nodeCount = reader.ReadUInt16();
+
+            foreach (var nodeIndex in Enumerable.Range(0, nodeCount))
             {
-                RailwaySpline spline = new()
+                var node = new RailwaySplineNode()
                 {
                     Name = reader.ReadPrefixedString(),
                     NameCrc = new Crc(reader.ReadUInt32()),
-                    Distance = reader.ReadSingle()
+                    StartPos = reader.ReadSingle(),
+                    Distance = reader.ReadSingle(),
+                    Pos = new Vector3(reader.ReadSingle(), reader.ReadSingle(), reader.ReadSingle()),
+                    Influence = new Vector3(reader.ReadSingle(), reader.ReadSingle(), reader.ReadSingle()),
+                    Tangent = new Quaternion(reader.ReadSingle(), reader.ReadSingle(), reader.ReadSingle(), reader.ReadSingle()),
+                    IsStart = reader.ReadUInt16() == 1,
+                    IsEnd = reader.ReadUInt16() == 1,
+                    IsStation = reader.ReadUInt16() == 1,
+                    MaxSpeed = reader.ReadSingle(),
+                    StationWaitTime = reader.ReadSingle(),
+                    TrainListBlueprintCrc = new Crc(reader.ReadUInt32())
                 };
-                int nodeCount = reader.ReadUInt16();
 
-                foreach (int nodeIndex in Enumerable.Range(0, nodeCount))
+                // Store hashes
+                Hash.FNV32string(node.Name);
+                Hash.StringToHash(node.Name);
+
+                var attachmentCount = reader.ReadInt32();
+                foreach (var attachmentIndex in Enumerable.Range(0, attachmentCount))
                 {
-                    RailwaySplineNode node = new()
+                    node.Attachments.Add(new RailwaySplineNodeAttachment()
                     {
-                        Name = reader.ReadPrefixedString(),
-                        NameCrc = new Crc(reader.ReadUInt32()),
-                        StartPos = reader.ReadSingle(),
-                        Distance = reader.ReadSingle(),
-                        Pos = new Vector3(reader.ReadSingle(), reader.ReadSingle(), reader.ReadSingle()),
-                        Influence = new Vector3(reader.ReadSingle(), reader.ReadSingle(), reader.ReadSingle()),
-                        Tangent = new Quaternion(reader.ReadSingle(), reader.ReadSingle(), reader.ReadSingle(), reader.ReadSingle()),
-                        IsStart = reader.ReadUInt16() == 1,
-                        IsEnd = reader.ReadUInt16() == 1,
-                        IsStation = reader.ReadUInt16() == 1,
-                        MaxSpeed = reader.ReadSingle(),
-                        StationWaitTime = reader.ReadSingle(),
-                        TrainListBlueprintCrc = new Crc(reader.ReadUInt32())
-                    };
-                    
-                    int attachmentCount = reader.ReadInt32();
-                    foreach (int attachmentIndex in Enumerable.Range(0, attachmentCount))
-                    {
-                        RailwaySplineNodeAttachment attachment = new()
-                        {
-                            SplineNameCrc = new Crc(reader.ReadUInt32()),
-                            NodeNameCrc = new Crc(reader.ReadUInt32())
-                        };
-                        node.Attachments.Add(attachment);
-                    }
-
-                    spline.Nodes.Add(node);
+                        SplineNameCrc = new Crc(reader.ReadUInt32()),
+                        NodeNameCrc = new Crc(reader.ReadUInt32())
+                    });
                 }
-                result.Add(spline);
-            }
-            return result;
-        }
 
-        public static void ExportGltfSplinePoints(List<RailwaySpline> railway, string outputPath)
+                spline.Nodes.Add(node);
+            }
+            result.Add(spline);
+        }
+        return result;
+    }
+
+    public static void ExportGltfSplinePoints(List<RailwaySpline> railway, string outputPath)
+    {
+        SceneBuilder scene = new();
+
+        MaterialBuilder material = new();
+
+        foreach (var spline in railway)
         {
-            SceneBuilder scene = new();
+            var mesh = new MeshBuilder<VertexPosition>(spline.Name);
+            var primitive = mesh.UsePrimitive(material, 2);
 
-            MaterialBuilder material = new();
+            RailwaySplineNode? previousNode = null;
 
-            foreach (RailwaySpline spline in railway)
+            foreach (var node in spline.Nodes)
             {
-                string modelName = spline.Name;
-                MeshBuilder<VertexPosition> mesh = new(modelName);
-                var primitive = mesh.UsePrimitive(material, 2);
-                RailwaySplineNode? previousNode = null;
-                foreach (RailwaySplineNode node in spline.Nodes)
-                {
-                    if (previousNode != null)
-                    {
-                        primitive.AddLine(new VertexPosition(previousNode.Pos), new VertexPosition(node.Pos));
-                    }
-                    previousNode = node;
-                }
-                scene.AddRigidMesh(mesh, Matrix4x4.Identity);
+                if (previousNode != null)
+                    primitive.AddLine(new VertexPosition(previousNode.Pos), new VertexPosition(node.Pos));
+
+                previousNode = node;
             }
 
-            ModelRoot model = scene.ToGltf2();
-            model.SaveGLTF(Path.Combine(outputPath, "railway.gltf"), new WriteSettings
-            {
-                Validation = SharpGLTF.Validation.ValidationMode.Strict
-            });
+            scene.AddRigidMesh(mesh, Matrix4x4.Identity);
         }
+
+        var model = scene.ToGltf2();
+        model.SaveGLTF(Path.Combine(outputPath, "railway.gltf"), new WriteSettings
+        {
+            Validation = SharpGLTF.Validation.ValidationMode.Strict
+        });
     }
 }
